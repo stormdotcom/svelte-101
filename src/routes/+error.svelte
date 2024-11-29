f174499e9844eb53ada82f20839e8bc4eff17306
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-11-29 18:16:32 +0530

    use

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,10 @@
 <script context="module" lang="ts">
-  import type { Load } from "@sveltejs/kit";
+  import { error, type Load } from "@sveltejs/kit";
 
-  export const load: Load = ({ status, error }) => {
-    console.error("Error in +error.svelte:", error);
+  export const load: Load = (o) => {
+    console.error(o)
     return {
-      status,
-      message: error?.message || "An unexpected error occurred.",
+      message:  "An unexpected error occurred.",
     };
   };
 </script>
@@ -15,14 +14,14 @@
   export let message: string = "Something went wrong";
 </script>
 
-<main class=" justify-center items-center min-h-screen">
+<main class="justify-center items-center min-h-screen">
   <div class="error-page justify-center text-center mb-8 flex">
     <h1 class="text-6xl text-red-500">{status}</h1>
     <div class="p-2 text-4xl">|</div>
     <p class="text-lg text-white-700">{message}</p>
   </div>
   <div class="flex justify-center">
-    <a href="/" class=" hover:underline mt-4 block">Go back to Home</a>
+    <a href="/" class="hover:underline mt-4 block">Go back to Home</a>
   </div>
   <footer class="text-center w-full mt-4">
     <p class="text-sm text-gray-200">
---
+++ src/routes/use/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Action } from 'svelte/action';
+
+	const gestures: Action<
+		HTMLDivElement,
+		null,
+		{
+			onswiperight: (e: CustomEvent) => void;
+			onswipeleft: (e: CustomEvent) => void;
+		}
+	> = (node) => {
+		// Setup swipe event dispatch
+		node.dispatchEvent(new CustomEvent('swipeleft'));
+		node.dispatchEvent(new CustomEvent('swiperight'));
+
+		// Teardown logic
+		return () => {
+			// Any teardown logic, e.g., remove event listeners
+		};
+	};
+</script>
+
+<div
+	use:gestures
+	onswipeleft={next}
+	onswiperight={prev}
+>...</div>
